f7c61b9f8fe2e7915f33ae79724269af1ba1c52a
HOSENUR <nur08439@gmail.com>
2023-02-23 12:57:35 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+//import space mono from next font
+import { Space_Mono } from "@next/font/google";
+
+const SpaceMono = Space_Mono({
+  subsets: ["latin"],
+  // weights: [400, 700],
+  display: "swap",
+  weight: ["400","700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={SpaceMono.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
