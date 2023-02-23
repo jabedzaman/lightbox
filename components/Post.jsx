a7d73979cdf7326fbef0146d75204e5b47371e16
HOSENUR <nur08439@gmail.com>
2023-02-23 15:36:52 +0530

    UI

--- components/Post.jsx
+++ components/Post.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Post({ user, post }) {
   return (
-    <div className={`max-w-6xl bg-white rounded   mx-auto p-5 text-3xl font-bold flex flex-col space-y-3`}>
+    <div className={` bg-white rounded   mx-auto p-5 text-3xl font-bold flex flex-col space-y-3`}>
       <div className="flex flex-row space-x-2 items-baseline">
         <div className="text-2xl">  Posted By : 0xfd70a206bade12af</div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -8,84 +8,62 @@ export default function Home() {
   const [user, setUser] = useState({ loggedIn: null });
 
   useEffect(() => fcl.currentUser.subscribe(setUser), []);
-
-  const AuthedState = () => {
-    const CreatePost = () => {
-      const [post, setPost] = useState("");
-      const user = fcl.currentUser().snapshot().addr;
-      const submitPost = async (e) => {
-        e.preventDefault();
-        const txId = await fcl
-          .send([
-            fcl.transaction`
-              import Profile from 0xProfile
-              transaction(post: String) {
-                prepare(account: AuthAccount) {
-                  let profile = Profile.init(account)
-                  profile.addPost(post: post)
-                }
-              }
-            `,
-            fcl.args([fcl.arg(post, fcl.types.String)]),
-            fcl.proposer(fcl.currentUser().authorization),
-            fcl.payer(fcl.currentUser().authorization),
-            fcl.authorizations([fcl.currentUser().authorization]),
-            fcl.limit(35),
-          ])
-          .then(fcl.decode);
-        console.log(txId);
-      };
-
-      return (
-        <div className="max-w-6xl mx-auto p-10 mt-10 border border-white flex flex-col">
-          <div>
-            <h1 className="text-4xl">Create Post</h1>
-          </div>
-          <div className="flex flex-row">
-            <input
-              className="w-full text-black focus:outline-none px-10 py-2 "
-              type="text"
-              placeholder="type here something..."
-            />
-            <button className="px-10 text-2xl" onClick={submitPost}>
-              Post
-            </button>
-          </div>
-        </div>
-      );
-    };
-    return (
-      <div>
-        <CreatePost />
-        <div className="p-10 flex flex-col space-y-4">
-          <Post
-            post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
-            user="hosenur"
-          />
-        </div>
-      </div>
-    );
-  };
-
-  const UnauthenticatedState = () => {
-    return (
-      <div className="max-w-7xl bg-white mx-auto p-10 text-9xl rounded-3xl min-h-screen flex items-center">
-        <span>
-          OOOOOOOPS ;( IT SEEMS LIKE YOU ARE NOT LOGGED IN, PLEASE{" "}
-          <span className="text-emerald-400">LOGIN</span> TO CONTINUE 😀
-        </span>
-      </div>
-    );
+  const postToFlow = async (post) => {
+    const txId = await fcl
+      .send([
+        fcl.transaction`
+        import Profile from 0xba1132bc08f82fe2
+        transaction(post: String) {
+          prepare(account: AuthAccount) {
+            let profile = Profile.init()
+            profile.save(post: post)
+            account.save(<-profile, to: /storage/Profile)
+            account.link<&Profile.Base{Profile.Public}>(/public/Profile, target: /storage/Profile)
+          }
+        }
+      `,
+        fcl.args([fcl.arg("hi", fcl.types.String)]),
+        fcl.proposer(fcl.authz),
+        fcl.payer(fcl.authz),
+        fcl.authorizations([fcl.authz]),
+        fcl.limit(35),
+      ])
+      .then(fcl.decode);
+    return txId;
   };
 
   return (
-    <div className="pt-20">
-      <Head>
-        <title>FCL Quickstart with NextJS</title>
-        <meta name="description" content="My first web3 app on Flow!" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      {user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
+    <div>
+      <div className="pt-20 flex flex-col space-y-4">
+        <button
+          className="text-white"
+          onClick={() => {
+            postToFlow("hello world");
+          }}
+        >
+          Post
+        </button>
+        <Post
+          post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
+          user="hosenur"
+        />
+        <Post
+          post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
+          user="hosenur"
+        />
+        <Post
+          post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
+          user="hosenur"
+        />
+        <Post
+          post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
+          user="hosenur"
+        />
+        <Post
+          post="piente id voluptates, laboriosam magni dolore eligendi impedit repellendus sed quo quod, dignissimos quisquam ad quasi numquam optio, earum natus totam quidem!"
+          user="hosenur"
+        />
+      </div>
     </div>
   );
 }
